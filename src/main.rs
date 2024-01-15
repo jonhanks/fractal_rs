5a2b7f14695d340adbc34e2f0f6af097bb41bcd6
Jonathan Hanks <jonathan.k.hanks@gmail.com>
2024-01-15 12:12:10 -0800

    Added multiple color schemes, allow changing the julia set while viewing it.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 mod mandelbrot;
+mod palette;
 
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
+use num_complex::Complex64;
+use crate::mandelbrot::FractalType;
+
+
+fn cycle_palette(p: palette::Palette) -> palette::Palette {
+    return match p.palette_type {
+        palette::PaletteType::BW => palette::new_color1_lin(),
+        palette::PaletteType::Color1Lin => palette::new_color1_mod(),
+        palette::PaletteType::Color1Mod => palette::new_bw(),
+    }
+}
 
 fn new_fractal(w: u32, h: u32) -> mandelbrot::Data {
     let state = mandelbrot::State {
@@ -17,27 +29,55 @@ fn new_fractal(w: u32, h: u32) -> mandelbrot::Data {
     mandelbrot::Data::new(state)
 }
 
-fn render_image_to_surface(fractal: &mandelbrot::Data, surface: &mut sdl2::surface::Surface) {
-    surface.with_lock_mut(|buffer| {
-        let mut offset = 0;
-        let scale_factor = 255.0/fractal.state.max_iterations as f64;
-        fractal.fractal_data.iter().for_each(|row| {
-            row.iter().for_each(|entry| {
-                if entry.escape >= fractal.state.max_iterations {
-                    buffer[offset] = 0;
-                    buffer[offset+1] = 0;
-                    buffer[offset+2] = 0;
-                    buffer[offset+3] = 0;
-                } else {
-                    let val = ((20 + (entry.escape as f64 * scale_factor) as i32) & 0xff) as u8;
-                    buffer[offset] = val;
-                    buffer[offset+1] = val;
-                    buffer[offset+2] = val;
-                    buffer[offset+2] = val;
-                }
-                offset += 4;
-            })
+fn u8slice_to_u32(input: &mut[u8]) -> &mut[u32] {
+    if input.len() == 0 || input.len() % 4 != 0 {
+        panic!("invalid length for u8 slice, not a multiple of 4");
+    }
+    let count = input.len() / 4;
+    unsafe {
+        let p = input.as_mut_ptr() as *mut u32;
+        std::slice::from_raw_parts_mut(p, count)
+    }
+}
+
+fn render_image_linear(fractal: &mandelbrot::Data, buffer: &mut[u32], pal :&palette::PaletteData) {
+    let mut offset = 0;
+    let scale_factor = (pal.len()-1) as f64/fractal.state.max_iterations as f64;
+    fractal.fractal_data.iter().for_each(|row| {
+        row.iter().for_each(|entry| {
+            if entry.escape >= fractal.state.max_iterations {
+                buffer[offset] = 0;
+            } else {
+                let val = (entry.escape as f64 * scale_factor) as usize;
+                buffer[offset] = pal[val];
+            }
+            offset += 1;
+        })
+    });
+}
+
+fn render_image_modulus(fractal: &mandelbrot::Data, buffer: &mut[u32], pal :&palette::PaletteData) {
+    let mut offset = 0;
+    let pal_len = pal.len();
+    fractal.fractal_data.iter().for_each(|row| {
+        row.iter().for_each(|entry| {
+            if entry.escape >= fractal.state.max_iterations {
+                buffer[offset] = 0;
+            } else {
+                buffer[offset] = pal[entry.escape as usize % pal_len];
+            }
+            offset += 1;
         });
+    });
+}
+
+fn render_image_to_surface(fractal: &mandelbrot::Data, surface: &mut sdl2::surface::Surface, pal: &palette::Palette) {
+    surface.with_lock_mut(|buffer| {
+        let mut buffer = u8slice_to_u32(buffer);
+        match pal.color_mode {
+            palette::ColorMode::LinearScale => render_image_linear(fractal, buffer, &pal.palette),
+            palette::ColorMode::Modulus => render_image_modulus(fractal, buffer, &pal.palette),
+        };
     });
 }
 
@@ -57,7 +97,9 @@ fn main() {
     canvas.present();
 
     let mut events = context.event_pump().unwrap();
-    let mut i = 0;
+
+    let mut pal = palette::new_color1_mod();
+
     let mut done = false;
     let mut do_redraw = true;
 
@@ -77,6 +119,11 @@ fn main() {
                 }
                 Event::KeyDown { keycode: Some(Keycode::Q), .. } => {
                     done = true;
+                }
+                Event::KeyDown { keycode: Some(Keycode::C), .. } => {
+                    pal = cycle_palette(pal);
+                    println!("New palette type is {:?} color mode is {:?}", pal.palette_type, pal.color_mode);
+                    do_redraw = true;
                 }
                 Event::KeyDown { keycode: Some(Keycode::Plus), .. } => {
                     fractal.state.scale = 0.9 * fractal.state.scale;
@@ -106,6 +153,34 @@ fn main() {
                     fractal.state.center.im -= 0.1*fractal.state.scale;
                     do_redraw = true;
                 }
+                Event::KeyDown { keycode: Some(Keycode::A), .. } => {
+                    if let FractalType::Julia(c) = fractal.state.fractal_type {
+                        let c = Complex64::new(c.re - 0.1*fractal.state.scale, c.im);
+                        fractal.state.fractal_type = FractalType::Julia(c);
+                        do_redraw = true;
+                    }
+                }
+                Event::KeyDown { keycode: Some(Keycode::D), .. } => {
+                    if let FractalType::Julia(c) = fractal.state.fractal_type {
+                        let c = Complex64::new(c.re + 0.1*fractal.state.scale, c.im);
+                        fractal.state.fractal_type = FractalType::Julia(c);
+                        do_redraw = true;
+                    }
+                }
+                Event::KeyDown { keycode: Some(Keycode::W), .. } => {
+                    if let FractalType::Julia(c) = fractal.state.fractal_type {
+                        let c = Complex64::new(c.re, c.im + 0.1*fractal.state.scale);
+                        fractal.state.fractal_type = FractalType::Julia(c);
+                        do_redraw = true;
+                    }
+                }
+                Event::KeyDown { keycode: Some(Keycode::S), .. } => {
+                    if let FractalType::Julia(c) = fractal.state.fractal_type {
+                        let c = Complex64::new(c.re, c.im - 0.1*fractal.state.scale);
+                        fractal.state.fractal_type = FractalType::Julia(c);
+                        do_redraw = true;
+                    }
+                }
                 Event::KeyDown { keycode: Some(Keycode::LeftBracket), .. } => {
                     fractal.state.max_iterations += 25;
                     do_redraw = true;
@@ -128,7 +203,7 @@ fn main() {
         }
         if do_redraw {
             mandelbrot::compute_mandelbrot(&mut fractal);
-            render_image_to_surface(&fractal, &mut surface);
+            render_image_to_surface(&fractal, &mut surface, &pal);
 
             let r1 = sdl2::rect::Rect::new(0, 0, fractal.state.width, fractal.state.height);
             let r2 = sdl2::rect::Rect::new(0, 0, fractal.state.width, fractal.state.height);
---
+++ src/palette.rs
@@ -0,0 +1,91 @@
+pub type PaletteData = Vec::<u32>;
+
+#[derive(Copy, Clone, Debug)]
+pub enum ColorMode {
+    LinearScale,
+    Modulus,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum PaletteType {
+    BW,
+    Color1Mod,
+    Color1Lin,
+}
+
+pub struct Palette {
+    pub palette_type: PaletteType,
+    pub color_mode: ColorMode,
+    pub palette: PaletteData,
+}
+
+
+pub fn new_bw() -> Palette {
+    let mut pd = Vec::new();
+    for i in 0u32..255 {
+        pd.push(
+            (i & 0xff) << 16 |
+                (i & 0xff) << 8 |
+                (i & 0xff)
+        );
+    }
+    Palette{
+        palette_type: PaletteType::BW,
+        color_mode: ColorMode::LinearScale,
+        palette: pd,
+    }
+}
+
+pub fn new_color1_mod() -> Palette {
+    let mut pd = Vec::new();
+    pd.append(&mut color_step(0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 100));
+    pd.append(&mut color_step(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 100));
+    pd.append(&mut color_step(0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 100));
+    pd.append(&mut color_step(1.0, 1.0, 0.0, 0.0, 0.0, 0.7, 100));
+    Palette{
+        palette_type: PaletteType::Color1Mod,
+        color_mode: ColorMode::Modulus,
+        palette: pd,
+    }
+}
+
+pub fn new_color1_lin() -> Palette {
+    let mut p = new_color1_mod();
+    p.palette_type = PaletteType::Color1Lin;
+    p.color_mode = ColorMode::LinearScale;
+    p
+}
+
+
+fn rgb_f64_to_rgb_u32(r: f64, g: f64, b: f64) -> u32 {
+    let mut sr = (r*255.0) as u32;
+    let mut sg = (g*255.0) as u32;
+    let mut sb = (b*255.0) as u32;
+    if sr > 255 {
+        sr = 255;
+    }
+    if sg > 255 {
+        sg = 255;
+    }
+    if sb > 255 {
+        sb = 255;
+    }
+    ((sr << 16) | (sg << 8) | (sb)) as u32
+}
+
+fn color_step(mut cur_r: f64, mut cur_g: f64, mut cur_b: f64, dest_r: f64, dest_g: f64, dest_b: f64, steps: usize) -> PaletteData {
+    let mut p = Vec::new();
+    let steps_f = steps as f64;
+    let delta_r = (dest_r - cur_r)/steps_f;
+    let delta_g = (dest_g - cur_g)/steps_f;
+    let delta_b = (dest_b - cur_b)/steps_f;
+
+    for i in 0..steps {
+        p.push(rgb_f64_to_rgb_u32(cur_r, cur_g, cur_b));
+        cur_r += delta_r;
+        cur_g += delta_g;
+        cur_b += delta_b;
+    }
+    p
+}
+
